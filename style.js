bdaabcd4665d0d09d9394a1faeed760dbb533904
Greenleaf542 <160301858+Greenleaf542@users.noreply.github.com>
2024-10-29 09:01:01 +0530

    Update style.js

--- style.js
+++ style.js
@@ -7,22 +7,6 @@ menuIcon.onclick =()=>{
   navbar.classList.toggle('active');
 };
 
-// bubble javascript
-const container = document.getElementById('home1');
-
-for (let i = 0; i < 10; i++) {
-  const bubble = document.createElement('div');
-  bubble.classList.add('bubble');
-  
-  
-  const size = Math.random() * 40 + 20;  
-  bubble.style.width = `${size}px`;
-  bubble.style.height = `${size}px`;
-  bubble.style.left = `${Math.random() * 100}vw`;  
-  bubble.style.animationDuration = `${Math.random() * 5 + 5}s`;  
-  bubble.style.animationDelay = `${Math.random() * 2}s`; 
-  container.appendChild(bubble);
-}
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
